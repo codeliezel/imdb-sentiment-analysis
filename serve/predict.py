ed7fdd3f711b4744cf89efdd5b2bcf76c2c748b0
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2021-03-13 13:45:29 +0100

    add completions to functions

--- serve/predict.py
+++ serve/predict.py
@@ -22,8 +22,8 @@ def model_fn(model_dir):
 
     # First, load the parameters used to create the model.
     model_info = {}
-    model_info_path = os.path.join(model_dir, 'model_info.pth')
-    with open(model_info_path, 'rb') as f:
+    model_info_path = os.path.join(model_dir, "model_info.pth")
+    with open(model_info_path, "rb") as f:
         model_info = torch.load(f)
 
     print("model_info: {}".format(model_info))
@@ -31,16 +31,17 @@ def model_fn(model_dir):
     # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = LSTMClassifier(
-        model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
+        model_info["embedding_dim"], model_info["hidden_dim"], model_info["vocab_size"]
+    )
 
     # Load the store model parameters.
-    model_path = os.path.join(model_dir, 'model.pth')
-    with open(model_path, 'rb') as f:
+    model_path = os.path.join(model_dir, "model.pth")
+    with open(model_path, "rb") as f:
         model.load_state_dict(torch.load(f))
 
     # Load the saved word_dict.
-    word_dict_path = os.path.join(model_dir, 'word_dict.pkl')
-    with open(word_dict_path, 'rb') as f:
+    word_dict_path = os.path.join(model_dir, "word_dict.pkl")
+    with open(word_dict_path, "rb") as f:
         model.word_dict = pickle.load(f)
 
     model.to(device).eval()
@@ -50,34 +51,35 @@ def model_fn(model_dir):
 
 
 def input_fn(serialized_input_data, content_type):
-    print('Deserializing the input data.')
-    if content_type == 'text/plain':
-        data = serialized_input_data.decode('utf-8')
+    print("Deserializing the input data.")
+    if content_type == "text/plain":
+        data = serialized_input_data.decode("utf-8")
         return data
     raise Exception(
-        'Requested unsupported ContentType in content_type: ' + content_type)
+        "Requested unsupported ContentType in content_type: " + content_type
+    )
 
 
 def output_fn(prediction_output, accept):
-    print('Serializing the generated output.')
+    print("Serializing the generated output.")
     return str(prediction_output)
 
 
 def predict_fn(input_data, model):
-    print('Inferring sentiment of input data.')
+    print("Inferring sentiment of input data.")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if model.word_dict is None:
-        raise Exception('Model has not been loaded properly, no word_dict.')
+        raise Exception("Model has not been loaded properly, no word_dict.")
 
     # TODO: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    review_words = review_to_words(input_data)
-    data_X, data_len = convert_and_pad(model.word_dict, review_words)
+    data_words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, data_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -94,10 +96,10 @@ def predict_fn(input_data, model):
     #       be a numpy array which contains a single integer which is either 1 or 0
 
     # Much credit to an explanation gotten from this blog:-
-    # https://dwood3.github.io/machinedeeplearning/2019/06/08/deploying-sentiment-analysis.html
 
-    result = None
-    result = model(data)
-    result = round(result.item())
+    with torch.no_grad():
+        res = model.forward(data)
+
+    result = np.round(res.numpy())
 
     return result
