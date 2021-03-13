d814f82772a3e59220a281713a89b458ea1718ae
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2021-03-13 13:47:06 +0100

    add completions to functions

--- serve/predict.py
+++ serve/predict.py
@@ -95,8 +95,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    # Much credit to an explanation gotten from this blog:-
-
     with torch.no_grad():
         res = model.forward(data)
 
